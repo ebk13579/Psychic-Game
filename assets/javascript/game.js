1147b3d4d9e3fbf5676822c253b75d86da1553dc
Evan Kozierachi <ekoz@Evans-Air.home>
2018-10-06 22:14:21 -0400

    Finalize

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,47 @@
-// $(document).ready(function() {
-// $('input').on('keydown', function(e) {
-// let x = e.keycode;
-// console.log(x)
-// });
+$(document).ready(function () {
+    console.log("Yass");
+    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var randomLetter = letters[Math.floor(Math.random() * 26)];
+    console.log(randomLetter);
+    var winCount = 0;
+    var guessCount = 0;
+    var loseCount = 0;
+    var letterGuess = [];
+    var reset = function () {
+        guessCount = 0;
+        letterGuess = [];
+        randomLetter = letters[Math.floor(Math.random() * 26) + 1];
+    }
+    $(document).on('keypress', function (e2) {
+        var inputLetter = e2.key.toUpperCase();
 
-// //   // Notice I didn't set $(".jumbotron") to a var this time?
-// //   // If you only plan to use that selector once it doesn't need to be a var
-// //   $("#random-button").on("click", function() {
+        console.log(inputLetter);
 
-// //     // ... we generate a random number
-// //     var random = Math.floor(Math.random() * 1000) + 1;
-
-// //     // ... and then dump the random number into our random-number div.
-// //     $("#random-number").text(random);
-
-// //   });
-
-// });
+        if (inputLetter === randomLetter) {
+            winCount = winCount + 1;
+            console.log("Win(s): " + winCount);
+            reset();
+        }
+        else {
+            letterGuess.push(inputLetter.toUpperCase());
+            guessCount = guessCount + 1;
+            console.log("Letter Guess: " + letterGuess);
+            console.log("Guess(es): " + guessCount);
+        }
+        if (guessCount > 9) {
+            loseCount = loseCount + 1;
+            console.log("Lose(es): " + loseCount);
+            reset();
+        }
+        var guessRemain = 10 - guessCount;
+        var html =
+            "<h1>Guess what letter I'm thinking of...</h1>" +
+            "<br>" +
+            "<h3>Wins: " + winCount + "</h3>" +
+            "<h3>Losses: " + loseCount + "</h3>" +
+            "<br>" +
+            "<h3>Guesses left: " + guessRemain + "</h3>" +
+            "<h3>Your guesses thus far: " + letterGuess.join(", ") + "</h3>";
+        document.getElementById("Game").innerHTML = html;
+    });
+});
